fabdf0ff4b29b30ec011e111b51f2b07b54316e4
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-04-29 21:00:02 -0300

    criando tabela e capturando os dados do servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud_app';
 
-  constructor(private _dialog: MatDialog) {}
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'experience',
+  ];
+  dataSource!: MatTableDataSource<any>;
 
-  openAddEditEmpForm(){
-    this._dialog.open(EmpAddEditComponent)
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log('ee' + res)
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
