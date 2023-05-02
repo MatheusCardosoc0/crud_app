fdb1ca339ffe69b64bad42d63f48305e4b6972cf
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-02 19:51:50 -0300

    finalizando implementação com nodejs

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -10,18 +10,18 @@ export class EmployeeService {
   constructor(private _http: HttpClient) { }
 
   addEmployee(data: any): Observable<any>{
-    return this._http.post("http://localhost:3000/employees", data)
+    return this._http.post("http://localhost:3333/new_user", data)
   }
 
   attEmployee(id: number,data: any): Observable<any>{
-    return this._http.put(`http://localhost:3000/employees/${id}`, data)
+    return this._http.put(`http://localhost:3333/user_update/${id}`, data)
   }
 
   getEmployeeList(): Observable<any>{
-    return this._http.get('http://localhost:3000/employees')
+    return this._http.get('http://localhost:3333/')
   }
 
   deleteEmployee(id: number){
-    return this._http.delete(`http://localhost:3000/employees/${id}`)
+    return this._http.delete(`http://localhost:3333/delete_user?id=${id}`)
   }
 }
